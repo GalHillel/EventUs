4cbe98a6d073a9d981b0cb729f49765da89710df
Ziv Morgan <ziv.morgan@gmail.com>
2024-01-17 21:33:11 +0200

    added controller for event

--- backend/event-us/src/modules/event/event.controller.ts
+++ backend/event-us/src/modules/event/event.controller.ts
@@ -4,10 +4,11 @@ import { CreateEventDto } from '../dto/event.dto';
 import { UserEvent } from './event.model';
 import {Id} from '../dto/id.dto'
 import { User } from '../user/user.model';
+import { UserService } from '../user/user.service';
 
 @Controller('events')
 export class EventController {
-  constructor(private readonly eventService: EventService) {}
+  constructor(private readonly eventService: EventService,private readonly userService: UserService) {}
 
   @Post()
   async createEvent(@Body() createEventDto: CreateEventDto): Promise<UserEvent> {
@@ -19,11 +20,11 @@ export class EventController {
     this.eventService.printAllEvents();
     return this.eventService.findAllEvents();
   }
-  /*
+  
   @Get(":id/users")
   async getEventUsers(@Param("id") _id: Id): Promise<User[]>{
-    return this.eventService.getUsersForEvent(_id);
-  }*/
+    return  this.eventService.getUserIds(_id).then((ids) => this.userService.getUsers(ids))
+  }
 
   // Implement other CRUD endpoints as needed
 }
--- backend/event-us/src/modules/event/event.module.ts
+++ backend/event-us/src/modules/event/event.module.ts
@@ -4,12 +4,13 @@ import { EventController } from './event.controller';
 import { EventService } from './event.service';
 import { UserEvent, UserEventSchema } from './event.model';
 import { User, UserSchema } from '../user/user.model';
+import { UserService } from '../user/user.service';
 
 @Module({
   imports: [MongooseModule.forFeature([
     { name: UserEvent.name, schema: UserEventSchema },
-    ])],
+    { name: User.name, schema: UserSchema } ])],
     controllers: [EventController],
-    providers: [EventService],
+    providers: [EventService, UserService],
   })
 export class EventModule {}
