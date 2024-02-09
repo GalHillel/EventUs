0942e2ed5292c4c0a31e3c1c3db5e1bb4895c2c7
Ziv Morgan <ziv.morgan@gmail.com>
2024-02-09 13:55:11 +0200

    removed unused code, User inherits from UserProfile

--- frontend/app/src/main/java/com/example/eventus/data/model/User.java
+++ frontend/app/src/main/java/com/example/eventus/data/model/User.java
@@ -2,21 +2,17 @@ package com.example.eventus.data.model;
 
 import java.util.ArrayList;
 
-public class User extends UserDisplay{
-    String email,password,bio;
-    int rating;
-    int num_ratings;
-    ArrayList<String> messages, events;
+public class User extends UserProfile{
+    String email,password;
+    ArrayList<String> messages;
 
     public User(String _id, String name, String user_type, String profile_pic, String email, String password) {
         super(_id,name,user_type,profile_pic);
+
         this.email = email;
         this.password = password;
         this.messages = new ArrayList<String>();
-        this.events = new ArrayList<String>();
-        this.num_ratings = 0;
-        this.rating = 0;
-        this.bio = "";
+
     }
 
 
--- frontend/app/src/main/java/com/example/eventus/data/model/UserProfile.java
+++ frontend/app/src/main/java/com/example/eventus/data/model/UserProfile.java
@@ -3,7 +3,6 @@ package com.example.eventus.data.model;
 import java.util.ArrayList;
 
 public class UserProfile extends UserDisplay{
-
     String bio;
     int rating;
     int num_ratings;
@@ -11,6 +10,11 @@ public class UserProfile extends UserDisplay{
 
     public UserProfile(String _id, String name, String user_type,String profile_pic) {
         super(_id, name, user_type,profile_pic);
+        this.events = new ArrayList<String>();
+        this.num_ratings = 0;
+        this.rating = 0;
+        this.bio = "";
+
     }
 
 
