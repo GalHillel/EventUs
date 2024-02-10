ef644c769257eb50b251018ad692a2f32029caac
Ziv Morgan <ziv.morgan@gmail.com>
2024-02-10 16:59:12 +0200

    added style

--- backend/event-us/src/modules/dto/user.dto.ts
+++ backend/event-us/src/modules/dto/user.dto.ts
@@ -15,11 +15,11 @@ export class UserDto{
   readonly num_ratings: number;
 }
 
-export class ParticipantDto extends OmitType(UserDto,['rating','num_ratings']){}{}
+export class ParticipantDto extends OmitType(UserDto,['rating','num_ratings']){}
 export class CreateUserDto extends OmitType(UserDto,['_id','profile_pic','messages','events','rating','num_ratings','bio']){}
 export class LoginUserDto extends PickType(UserDto,['email','password','user_type']){}
 export class SearchUserDto extends PickType(UserDto,['_id','name','email','user_type']){}
-export class EditUserDto extends PickType(UserDto,['name','email','password','profile_pic','rating','num_ratings']){
+export class EditUserDto extends PickType(UserDto,['name','email','password','bio','profile_pic','rating','num_ratings','messages']){
   readonly oldPassword: string;
 }
 
